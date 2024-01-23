fb6eaba85ebee126e28ecf18e37f1dc92d90e7bc
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-01-22 23:07:45 -0300

    feat: starting dasboard-layout

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from "@/app/dashboard/components/NavBar";
+import Sidebar from "@/app/dashboard/components/SideBar";
+// import HeaderStats from "components/Headers/HeaderStats.js";
+// import FooterAdmin from "components/Footers/FooterAdmin.js";
+
+export default function Admin({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Sidebar />
+      <div className="relative md:ml-64 bg-blue-100">
+        <Navbar />
+        {/* <HeaderStats /> */}
+        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+          {children}
+          {/* <FooterAdmin /> */}
+        </div>
+      </div>
+    </>
+  );
+}
