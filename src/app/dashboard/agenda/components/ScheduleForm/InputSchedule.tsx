c36a0c790e6e01e3bf6616127ef62299d76abab9
RodrLucas <lucasrodriguessantos0@gmail.com>
2024-01-31 17:07:54 -0300

    feat: Finalizing the schedule form

--- src/app/dashboard/agenda/components/ScheduleForm/InputSchedule.tsx
+++ src/app/dashboard/agenda/components/ScheduleForm/InputSchedule.tsx
@@ -1,23 +1,22 @@
+import { HTMLInputTypeAttribute } from "react";
+
 export default function InputSchedule({
   label,
   type,
   placeholder,
 }: {
   label: string;
-  type: string;
-  placeholder: string;
+  type: HTMLInputTypeAttribute;
+  placeholder?: string;
 }) {
   return (
     <div className="relative mb-3 w-full">
-      <label
-        className="text-blueGray-600 mb-2 block text-xs font-bold uppercase"
-        htmlFor="grid-password"
-      >
+      <label className="text-blueGray-600 mb-2 block text-xs font-bold uppercase">
         {label}
       </label>
       <input
         // {...register("")}
-        placeholder={placeholder}
+        placeholder={placeholder ?? ""}
         type={type}
         className="text-blueGray-600 w-full rounded border-0 bg-white px-3 py-3 text-sm shadow transition-all duration-150 ease-linear"
       />
--- src/app/dashboard/agenda/components/ScheduleForm/ScheduleForm.tsx
+++ src/app/dashboard/agenda/components/ScheduleForm/ScheduleForm.tsx
@@ -3,12 +3,13 @@
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import InputSchedule from "./InputSchedule";
 
 const scheduleFormSchema = Yup.object().shape({
   scheduleDate: Yup.date()
     .required("Por favor, insira a data de agendamento")
     .typeError("Por favor, insira uma data válida"),
-  client: Yup.string().required()
+  client: Yup.string().required(),
 });
 
 export function ScheduleForm() {
@@ -30,66 +31,38 @@ export function ScheduleForm() {
       <form onSubmit={handleSubmit(handleScheduleForm)}>
         <div className="flex flex-wrap">
           <div className="w-full px-4 lg:w-6/12">
-            <div className="relative mb-3 w-full">
-              <label
-                className="text-blueGray-600 mb-2 block text-xs font-bold uppercase"
-                htmlFor="grid-password"
-              >
-                Data
-              </label>
-              <input
-                // {...register("")}
-                type="date"
-                className="text-blueGray-600 w-full rounded border-0 bg-white px-3 py-3 text-sm shadow transition-all duration-150 ease-linear"
-              />
-            </div>
+            <InputSchedule label="Data" type="date" />
           </div>
           <div className="w-full px-4 lg:w-6/12">
-            <div className="relative mb-3 w-full">
-              <label
-                className="text-blueGray-600 mb-2 block text-xs font-bold uppercase"
-                htmlFor="grid-password"
-              >
-                Horário de início
-              </label>
-              <input
-                // {...register("")}
-                type="time"
-                className="text-blueGray-600 w-full rounded border-0 bg-white px-3 py-3 text-sm shadow transition-all duration-150 ease-linear"
-              />
-            </div>
+            <InputSchedule label="Horário de início" type="time" />
           </div>
-          <div className="w-full px-4 lg:w-11/12">
-            <div className="relative mb-3 w-full">
-              <label
-                className="text-blueGray-600 mb-2 block text-xs font-bold uppercase"
-                htmlFor="grid-password"
-              >
-                Prestador de serviço
-              </label>
-              <input
-                // {...register("")}
-                placeholder="Prestador de serviço"
-                type="text"
-                className="text-blueGray-600 w-full rounded border-0 bg-white px-3 py-3 text-sm shadow transition-all duration-150 ease-linear"
-              />
-            </div>
+          <div className="w-full px-4 lg:w-6/12">
+            <InputSchedule
+              label="Prestador de serviço"
+              type="text"
+              placeholder="Informe o nome do Prestador de serviço"
+            />
           </div>
-          <div className="w-full px-4 lg:w-11/12">
-            <div className="relative mb-3 w-full">
-              <label
-                className="text-blueGray-600 mb-2 block text-xs font-bold uppercase"
-                htmlFor="grid-password"
-              >
-                Cliente
-              </label>
-              <input
-                // {...register("")}
-                placeholder="Cliente"
-                type="text"
-                className="text-blueGray-600 w-full rounded border-0 bg-white px-3 py-3 text-sm shadow transition-all duration-150 ease-linear"
-              />
-            </div>
+          <div className="w-full px-4 lg:w-6/12">
+            <InputSchedule
+              label="Cliente"
+              type="text"
+              placeholder="Informe o nome do cliente"
+            />
+          </div>
+          <div className="w-full px-4 lg:w-6/12">
+            <InputSchedule
+              label="Serviços"
+              type="text"
+              placeholder="Informe o tipo de serviço"
+            />
+          </div>
+          <div className="w-full px-4 lg:w-4/12">
+            <InputSchedule
+              label="Preço"
+              type="number"
+              placeholder="Preço do serviço"
+            />
           </div>
         </div>
       </form>
